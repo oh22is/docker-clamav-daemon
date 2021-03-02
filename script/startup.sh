9035105fcd56c41f80079c06c7d6146bcbffd743
Lucas Pribbenow <l.pribbenow@oh22.net>
2021-03-02 13:59:36 +0100

    edited startup script

--- script/startup.sh
+++ script/startup.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Set environment variable which points to the main ClamAV virus definition file
-MAIN_FILE="/var/lib/clamav/main.cvd"
-
-# Start freshclam to download the latest virus definitions
-if [ ! -f ${MAIN_FILE} ]; then
-    /usr/bin/freshclam
-fi
-
 # Start freshclam as a daemon
 freshclam -d
 
